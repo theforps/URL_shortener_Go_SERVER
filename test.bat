960685e2a11e1094846e1c5e3acce107e85707b9
well - wisher <theforps@gmail.com>
2026-03-19 16:18:00 +0300

    Update test.bat to simplify echo statements for database operations
    - Changed echo statements to remove "TEST" from the messages, making them more concise.
    - Updated the script to reflect the streamlined terminology for database operations during testing.

--- test.bat
+++ test.bat
@@ -1,11 +1,11 @@
 @echo off
-echo ######## TEST DB UP ########
+echo ######## DB UP ########
 echo docker-compose -f docker-compose.test.yml up -d
 
 echo ######## RUN TESTS ########
 go clean -testcache
 go test ./tests -v
 
-echo ######## TEST DB DOWN ########
+echo ######## DB DOWN ########
 echo docker-compose -f docker-compose.test.yml down
 pause
